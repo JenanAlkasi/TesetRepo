15b42e208542195fe6702500a32c57f11c54d3c3
JenanAlkasi <jenan.alkasi@icloud.com>
2025-07-16 02:07:39 +0300

    Add files via upload

---
+++ jenanlab4/src/jenanlab4/Jenanlab4.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package jenanlab4;
+
+/**
+ *
+ * @author jenan
+ */
+public class Jenanlab4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Wellcome to week4-lab");
+        int R1=1;
+        int R2=0;
+        int R3=0;
+        int R4=1;
+        int R5=2;
+        System.out.println("your lab eva is");
+        System.out.println(R1 + R2 + R3 + R4 + R5 + "out of 5" );
+        
+    }
+    
+}
